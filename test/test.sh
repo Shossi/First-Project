6b6ac8cd74f5d7703b57c5761abf985193281393
yossizxc <yossizxc@gmail.com>
2020-12-26 16:13:40 +0200

    added test dir

---
+++ test/test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+check_result() {
+  RESULT=$?
+  MESSAGE=$1
+  if [ $RESULT == 0 ]; then
+    echo [SUCCESS] $MESSAGE
+  else
+    echo [FAIL] $MESSAGE
+    exit 1
+  fi
+}
+
+list = ['israel','london','new york']
+
+for city in list;do
+  sudo docker run weatherapi -c city | grep weather
+  check_result $city
+done
