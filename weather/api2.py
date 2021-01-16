153ddfe5d360f1dae770ead187e0c42a7048d91b
yossizxc <yossizxc@gmail.com>
2021-01-17 01:28:25 +0200

    flask

--- weather/api2.py
+++ weather/api2.py
@@ -1,32 +1,9 @@
+from flask import Flask
+from flask import request
 import json
-# import ./load.py
-import argparse
 import send
 
-
-
-# import requests
-# global key, host
-
-
-#
-# def send(key, host, city):
-#     url = "https://community-open-weather-map.p.rapidapi.com/forecast/daily"  # open weather url endpoint
-#     querystring = {"q": city, "units": "metric"}  # my query
-#     headers = {
-#         'x-rapidapi-host': host,
-#         'x-rapidapi-key': key
-#     }
-#
-#     response = requests.request("GET", url, headers=headers, params=querystring)
-#     response = response.json()
-# #   print(response)
-#     city1 = response['city']['name']
-#     day = response['list'][0]['temp']['day']
-#     night = response['list'][0]['temp']['night']
-#     weather = response['list'][0]['weather'][0]['description']
-#     print("weather in", city1, "today:\nday temperature:", day, "\nnight temperature:", night, '\nweather:', weather)
-
+app = Flask(__name__)
 
 def load_config():
     with open('config.json') as json_file:
@@ -35,15 +12,17 @@ def load_config():
         host = configs['config']['host']
         return key, host
 
+@app.route('/', methods=['POST'])
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--city', action='store', dest='city', help='Weather and temp report of given city', required=True)
-    result = parser.parse_args()
-    city = result.city
+    data = request.get_json() or {}
+    country = data['country']
     key = load_config()[0]
     host = load_config()[1]
-    send.send(key, host, city)
-
-
-main()
+    response = send.send(key, host, country)
+    print(response)
+    city1 = response['city']['name']
+    day = response['list'][0]['temp']['day']
+    night = response['list'][0]['temp']['night']
+    weather = response['list'][0]['weather'][0]['main']
+    return"weather in", city1, "today:\nday temperature:", day, "\nnight temperature:", night, '\nweather:', weather
--- weather/send.py
+++ weather/send.py
@@ -1,9 +1,9 @@
 import requests
 
 
-def send(key, host, city):
+def send(key, host, country):
     url = "https://community-open-weather-map.p.rapidapi.com/forecast/daily"  # open weather url endpoint
-    querystring = {"q": city, "units": "metric"}  # my query
+    querystring = {"q": country, "units": "metric"}  # my query
     headers = {
         'x-rapidapi-host': host,
         'x-rapidapi-key': key
@@ -12,8 +12,9 @@ def send(key, host, city):
     response = requests.request("GET", url, headers=headers, params=querystring)
     response = response.json()
 #print(response)
-    city1 = response['city']['name']
-    day = response['list'][0]['temp']['day']
-    night = response['list'][0]['temp']['night']
-    weather = response['list'][0]['weather'][0]['main']
-    print("weather in", city1, "today:\nday temperature:", day, "\nnight temperature:", night, '\nweather:', weather)
+    # city1 = response['city']['name']
+    # day = response['list'][0]['temp']['day']
+    # night = response['list'][0]['temp']['night']
+    # weather = response['list'][0]['weather'][0]['main']
+    # print("weather in", city1, "today:\nday temperature:", day, "\nnight temperature:", night, '\nweather:', weather)
+    return response
