e277c295800b4b4b185625c89f48c7dfc437624b
yossizxc <yossizxc@gmail.com>
2021-01-21 05:13:04 +0200

    ,

--- weather/test/test.sh
+++ weather/test/test.sh
@@ -14,7 +14,7 @@ check_result () {
 
 containerName=$(echo $TAG | cut -d '-' -f2)
 sudo docker rm -f $(sudo docker ps -aq)
-sudo docker run -d -p 5000:5000 "$containerName"
+sudo docker run -d -p 5000:5000 --name "$containerName" "$TAG"
 sleep 5
 for country in israel; do
   curl -X POST --header "Content-Type: application/json" -d "{\"country\":\"$country\"}" http://localhost:5000
