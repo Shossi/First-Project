a74c4c589f54ba1d6435b1a84e7e06e028876610
yossizxc <yossizxc@gmail.com>
2021-01-21 05:02:58 +0200

    file location mistake addtion to last commit

--- weather/test/test.sh
+++ weather/test/test.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
-
+TAG=$1
 
 check_result () {
-    if [ $?==0 ]; then
+  RESULT=$?
+  MESSAGE=$1
+    if [ $RESULT==0 ]; then
         echo [SUCCESS]
     else
         echo [FAIL]
         exit 1
     fi
 }
-for city in israel london;do
-	sudo docker run weatherapi -c $city
-	check_result $city
+
+containerName=$(echo $TAG | cut -d '-' -f2)
+sudo docker rm -f $(sudo docker ps -aq)
+sudo docker run -d -p 5000:5000 "$containerName"
+sleep 5
+for country in israel; do
+  curl -X POST --header "Content-Type: application/json" -d "{\"country\":\"$country\"}" http://localhost:5000
+  check_result $country
 done
+
 exit 0
